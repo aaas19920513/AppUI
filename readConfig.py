bbd9a825b111d82a97d2c62d2ecdea034d2e8f7e
tuihou <345203857@qq.com>
2017-12-07 23:49:23 +0800

    Did some fixed

--- readConfig.py
+++ readConfig.py
@@ -1,25 +1,15 @@
-import os
+# -*-coding:utf-8-*-
+from config import globalparameter as gl
 import configparser
-import codecs
 global configfile_path
 
-prjDir = os.path.split(os.path.realpath(__file__))[0]
-configfile_path = os.path.join(prjDir, "config.ini")
+# 获取配置文件路径
+configfile_path = gl.configfile_path
 
 
 class ReadConfig:
+
     def __init__(self):
-
-        fd = open(configfile_path)
-        data = fd.read()
-        # remove BOM
-        if data[:3] == codecs.BOM_UTF8:
-            data = data[3:]
-            file = codecs.open(configfile_path, "w")
-            file.write(data)
-            file.close()
-        fd.close()
-
         self.cf = configparser.ConfigParser()
         self.cf.read(configfile_path)
 
